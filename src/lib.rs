394ff8fa7be31d77a3bb98eb9eace931f016e146
Derrick Plotsky <derrickplotsky@gmail.com>
2023-06-27 17:44:52 -0700

    Remove user for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
+pub mod api;
 pub mod config;
 pub mod landmarks;
 pub mod minecraft;
 pub mod persistence;
 mod string_visitor;
 mod tag;
-mod user;
 pub mod worlds;
 
 pub use string_visitor::StringVisitor;
 pub use tag::Tag;
-pub use user::User;
--- src/user.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct User {
-    pub id: Uuid,
-    pub name: String,
-}
