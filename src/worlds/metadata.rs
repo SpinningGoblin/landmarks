9d76c31988aac404246c6d44d16d53e476888545
Derrick Plotsky <derrickplotsky@gmail.com>
2023-06-27 17:47:14 -0700

    Remove world for now
    I don't know that this will actually be that useful to me. I'll
    probably end up just returning the chunks of this from the endpoints
    instead of the full world.

--- src/worlds/metadata.rs
+++ src/worlds/metadata.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
 use crate::{
     minecraft::{Platform, Seed},
@@ -6,7 +7,8 @@ use crate::{
 };
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Metadata {
+pub struct WorldMetadata {
+    pub id: Uuid,
     pub seed: Seed,
     #[serde(default)]
     pub name: Option<String>,
@@ -15,32 +17,5 @@ pub struct Metadata {
     pub platform: Platform,
     #[serde(default)]
     pub notes: Option<String>,
-}
-
-impl From<Seed> for Metadata {
-    fn from(value: Seed) -> Self {
-        Self {
-            name: Some(value.0.clone()),
-            seed: value,
-            tags: Vec::new(),
-            platform: Platform::Bedrock,
-            notes: None,
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::minecraft::Seed;
-
-    use super::Metadata;
-
-    #[test]
-    pub fn serialize() {
-        let seed = Seed("1112222".to_string());
-        let metadata = Metadata::from(seed);
-        let expected = "{\"seed\":\"1112222\",\"name\":\"1112222\",\"tags\":[],\"platform\":\"bedrock\",\"notes\":null}";
-        let serialized = serde_json::to_string(&metadata).unwrap();
-        assert_eq!(expected, serialized);
-    }
+    pub creator: String,
 }
--- src/worlds/mod.rs
+++ src/worlds/mod.rs
@@ -1,7 +1,5 @@
 mod create_world;
 mod metadata;
-mod world;
 
 pub use create_world::CreateWorld;
-pub use metadata::Metadata;
-pub use world::World;
+pub use metadata::WorldMetadata;
