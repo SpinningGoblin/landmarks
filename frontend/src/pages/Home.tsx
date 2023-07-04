701bb72ace743b2843ec00674de96fcd6c6b53a3
Derrick Plotsky <derrickplotsky@gmail.com>
2023-07-03 18:04:13 -0700

    Do some cleanup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react";
 import { useWorlds } from "../hooks/worlds";
-import { Container, Stack } from "@mui/material";
+import { Container, Stack, Typography } from "@mui/material";
 import { WorldCard } from "../components/WorldCard";
 
 export const Home: FC = () => {
-  const { worlds } = useWorlds();
+  const { worlds, isLoading } = useWorlds();
 
   return (
     <Container>
       <Stack spacing={2}>
+        {isLoading && <Typography>Loading ...</Typography>}
         {worlds?.map((world) => (
           <WorldCard key={world.id} world={world} />
         ))}
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,4 +2,3 @@ export { Home } from "./Home";
 export { SignIn } from "./SignIn";
 export { World } from "./World";
 export { AddLandmark } from "./AddLandmark";
-export { AuthenticatedPage } from "./AuthenticatedPage";
