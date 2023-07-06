66e12479b5d67f43f67b0be89491bed2fe41586b
Derrick Plotsky <derrickplotsky@gmail.com>
2023-07-05 22:48:43 -0700

    Add AddWorld page

---
+++ frontend/src/pages/AddWorld.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+import { useAddWorld, useNavigateToWorld } from "../hooks/worlds";
+import { Paper, Stack, Typography } from "@mui/material";
+import { WorldForm } from "../components/WorldForm";
+import { buildEmptyEditingWorld } from "../models/EditingWorld";
+
+export const AddWorld: FC = () => {
+  const navigateToWorld = useNavigateToWorld();
+  const { addWorld } = useAddWorld(navigateToWorld);
+
+  if (addWorld.isLoading) {
+    return <Typography variant="subtitle1">Saving ...</Typography>;
+  }
+
+  const world = buildEmptyEditingWorld();
+
+  return (
+    <Paper>
+      <Stack spacing={2}>
+        <Typography variant="h5">Add New World</Typography>
+        <WorldForm world={world} onFormSubmit={addWorld.mutate} />
+      </Stack>
+    </Paper>
+  );
+};
