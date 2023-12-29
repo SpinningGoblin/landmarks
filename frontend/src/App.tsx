c1d51f5a817ef39fed1f0a299d50916d57039cd6
Derrick Plotsky <derrickplotsky@gmail.com>
2023-12-28 19:39:00 -0800

    Fix a lint error

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -30,36 +30,32 @@ export const App: FC<AppProps> = ({ startingUser }) => {
   const signedIn = !!user;
 
   return (
-    <>
-      <AuthProvider value={user}>
-        {!signedIn && (
-          <SignIn
-            userChanged={(user) => {
-              setUser(user);
-              navigate("");
-            }}
+    <AuthProvider value={user}>
+      {!signedIn && (
+        <SignIn
+          userChanged={(user) => {
+            setUser(user);
+            navigate("");
+          }}
+        />
+      )}
+      {signedIn && (
+        <Routes>
+          <Route
+            path="/worlds/:worldId/add_landmark"
+            element={<AddLandmark />}
           />
-        )}
-        {signedIn && (
-          <>
-            <Routes>
-              <Route
-                path="/worlds/:worldId/add_landmark"
-                element={<AddLandmark />}
-              />
-              <Route path="/add_world" element={<AddWorld />} />
-              <Route path="/worlds/:worldId/share" element={<ShareWorld />} />
-              <Route path="/worlds/:worldId" element={<World />} />
-              <Route
-                path="worlds/:worldId/landmarks/:landmarkId"
-                element={<Landmark />}
-              />
-              <Route path="/worlds" element={<Home />} />
-              <Route path="" element={<Home />} />
-            </Routes>
-          </>
-        )}
-      </AuthProvider>
-    </>
+          <Route path="/add_world" element={<AddWorld />} />
+          <Route path="/worlds/:worldId/share" element={<ShareWorld />} />
+          <Route path="/worlds/:worldId" element={<World />} />
+          <Route
+            path="worlds/:worldId/landmarks/:landmarkId"
+            element={<Landmark />}
+          />
+          <Route path="/worlds" element={<Home />} />
+          <Route path="" element={<Home />} />
+        </Routes>
+      )}
+    </AuthProvider>
   );
 };
